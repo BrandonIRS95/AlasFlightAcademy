9eb18417e79ba2c0676c59abd3a2e2a2a867d626
Unknown <brandonreyes230495@gmail.com>
2016-10-22 13:55:59 -0700

    Actualizar eventos cada 5 minutos

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:events')->everyMinute();
+        $schedule->command('update:events')->everyFiveMinutes();
     }
 
     /**
