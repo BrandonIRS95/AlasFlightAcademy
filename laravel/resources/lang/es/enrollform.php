e32c5fcc43ff5e117dbd82a590bc6183c0ce8aa9
Unknown <brandonreyes230495@gmail.com>
2016-10-25 20:03:22 -0700

    About, Programs, Contact, Become, Enroll traducidas, solo lo seguro
    De enroll falta traducir el formulario

---
+++ laravel/resources/lang/es/enrollform.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'steps_enroll' => 'Pasos para inscrib&iacute;rte',
+  'step1_description' => 'Completa la solicitud de inscripci&oacute;n.',
+  'step2_description' => 'Presentar una cuota de $150 d&oacute;lares. Aceptamos Visa, Mastercard, American Express, Discover o transferencia bancaria. (Por favor, p&oacute;ngase en contacto con Admisiones para instrucciones de pago).',
+  'step3_description' => 'Presentar un dep&oacute;sito de alojamiento de $300 d&oacute;lares. S&oacute;lo para aquellos que quieren un dormitorio en el campus. Disponibilidad limitada.',
+  'step4_description' => '<p class="Enroll__step__description">Proporcionar a la academia una copia de:
+      <ul class="Enroll__step__description__list">
+        <li class="Enroll__step__description__item">Un 1er o 2do FFA certificado m&eacute;dico clase.</li>
+        <li class="Enroll__step__description__item">Diploma de escuela secundaria o equivalente.</li>
+        <li class="Enroll__step__description__item">Identificaci&oacute;n de gobierno.</li>
+      </ul>
+      </p>',
+  'enroll' => 'Inscr&iacute;bete'
+  ];
--- laravel/resources/lang/es/messages.php
+++ laravel/resources/lang/es/messages.php
@@ -1,10 +1,27 @@
 <?php
 
 return [
-    'about' => 'Acerca',
+    'about' => 'Con&oacute;cenos',
     'pilot_programs' => 'Programas',
-    'contact' => 'Contacto',
+    'contact' => 'Cont&aacute;ctanos',
     'become_a_pilot' => 'Convi&eacute;rtete en piloto',
-    'enroll_now' => 'Enl&iacute;state ahora',
+    'enroll_now' => 'Inscr&iacute;bete ahora',
     'signin' => 'Ingresar',
+    'spread_wings' => 'Despliega tus alas',
+    'want_become_pilot' => '¿Qui&eacute;res ser un piloto',
+    'apply_now' => 'Inscr&iacute;bete ya',
+    'make_dream_true' => 'Haz tu sue&ntilde;o realidad',
+    'contact_us' => 'Cont&aacute;ctanos',
+    'have_questions' => '¿Preguntas',
+    'history' => 'Historia',
+    'about_founder' => 'Acerca del fundador',
+    'title_contact' => 'Estaremos encantados de ayudarte a convertirte en un piloto',
+    'first_name' => 'Nombre(s)',
+    'last_name' => 'Apellidos',
+    'phone_number' => 'Tel&eacute;fono celular',
+    'optional' => 'opcional',
+    'email' => 'Correo electr&oacute;nico',
+    'comments_or_questions' => 'Comentarios o preguntas',
+    'send' => 'Enviar',
+    'our_programs' => 'Nuestros programas'
 ];
