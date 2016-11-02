e793e2ec3a11cc0356b157c79924b37a40724969
Unknown <brandonreyes230495@gmail.com>
2016-11-02 13:43:14 -0700

    Se muestra el clima real, falta animacion

--- laravel/public/js/calendar/weather.js
+++ laravel/public/js/calendar/weather.js
@@ -1,5 +1,6 @@
 $('#show-weather-btn').click(function () {
   var $conteiner = $('#conteiner-weather');
+    var hourFormat = $('#flight_start_hour').val() + ':' + $('#flight_start_minute').val() + ':00';
   var template = '<img id="cloud6" src="svg/calendar/cloud6.svg"/>' +
   '<img id="cloud5" src="svg/calendar/cloud5.svg"/>' +
   '<img id="cloud4" src="svg/calendar/cloud4.svg"/>' +
@@ -8,6 +9,25 @@ $('#show-weather-btn').click(function () {
   '<img id="cloud3" src="svg/calendar/cloud3.svg"/>' +
   '<img id="plane-svg" src="svg/calendar/plane.svg"/>';
   $conteiner.append(template);
+
+    $.ajax({
+        url: 'https://api.darksky.net/forecast/5b3325c22c6254d2ba685f4a47343d3e/32.530833,-117.02?time=' + SELECTED_DATE.toString('yyyy-MM-dd') + 'T'+hourFormat+'&exclude=minutely,hourly,daily',
+        method: 'GET',
+        dataType: "jsonp"
+    }).done(function (response) {
+        var currently = response.currently;
+        var $temp = $('#temperature');
+        var $wind = $('#wind');
+        var $summary = $('#summary');
+        var tempFormat = '';
+        var c = (currently.temperature - 32) / 1.8;
+        tempFormat = c.toFixed(0) + ' &deg;C | ' + currently.temperature.toFixed(0) + ' &deg;F';
+        $temp.html(tempFormat);
+        $wind.html(currently.windSpeed + ' MPH');
+        $summary.html(currently.summary);
+        $('#imgSummary').attr('src', urlSvgCalendar + currently.icon + '.svg')
+
+    });
 
   TweenMax.to($conteiner, 0.4, {top: 0, onComplete: function () {
 
--- laravel/resources/views/includes/weather.blade.php
+++ laravel/resources/views/includes/weather.blade.php
@@ -3,9 +3,9 @@
         <img src="{{URL::to('svg/ic_close_black_24px.svg')}}">
     </div>
     <div id="content-weather">
-        <div class="prop-weather"><img class="icon" src="{{URL::to('svg/calendar/43.svg')}}"><span id="temperature">31 &deg;C | 65 &deg;F</span></div>
-        <div class="prop-weather"><img class="icon" src="{{URL::to('svg/calendar/8.svg')}}"><span id="prediction">Cloudy</span></div><br>
-        <div class="prop-weather" style="clear: both;"><img class="icon" src="{{URL::to('svg/calendar/44.svg')}}"><span id="wind">14-19 mph</span></div>
+        <div class="prop-weather"><img class="icon" src="{{URL::to('svg/calendar/43.svg')}}"><span id="temperature">...</span></div>
+        <div class="prop-weather"><img id="imgSummary" class="icon" src="{{URL::to('svg/calendar/ic_hourglass_empty_white_24px.svg')}}"><span id="summary">...</span></div><br>
+        <div class="prop-weather" style="clear: both;"><img class="icon" src="{{URL::to('svg/calendar/44.svg')}}"><span id="wind">...</span></div>
     </div>
 </div>
 <div id="show-weather-btn">
