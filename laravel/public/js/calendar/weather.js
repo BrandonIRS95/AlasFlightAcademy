77ff35829b914bc46e7c5048b42c5258dc95dc5e
Unknown <brandonreyes230495@gmail.com>
2016-11-09 15:37:05 -0800

    Resuelto bug al dar multiple clicks al boton de mostrar el clima

--- laravel/public/js/calendar/weather.js
+++ laravel/public/js/calendar/weather.js
@@ -1,16 +1,19 @@
 var goRain = true;
 
-$('#show-weather-btn').click(function () {
-  var $conteiner = $('#conteiner-weather');
+$('#show-weather-btn').on('click', showWeather);
+
+function showWeather() {
+    $('#show-weather-btn').off('click');
+    var $conteiner = $('#conteiner-weather');
     var hourFormat = $('#flight_start_hour').val() + ':' + $('#flight_start_minute').val() + ':00';
-  var template = '<img id="cloud6" class="weather" src="svg/calendar/cloud6.svg"/>' +
-  '<img id="cloud5" class="weather" src="svg/calendar/cloud5.svg"/>' +
-  '<img id="cloud4" class="weather" src="svg/calendar/cloud4.svg"/>' +
-  '<img id="cloud1" class="weather" src="svg/calendar/cloud1.svg"/>' +
-  '<img id="cloud2" class="weather" src="svg/calendar/cloud2.svg"/>' +
-  '<img id="cloud3" class="weather" src="svg/calendar/cloud3.svg"/>' +
-  '<img id="plane-svg" class="plane" src="svg/calendar/plane.svg"/>';
-  $conteiner.append(template);
+    var template = '<img id="cloud6" class="weather" src="svg/calendar/cloud6.svg"/>' +
+        '<img id="cloud5" class="weather" src="svg/calendar/cloud5.svg"/>' +
+        '<img id="cloud4" class="weather" src="svg/calendar/cloud4.svg"/>' +
+        '<img id="cloud1" class="weather" src="svg/calendar/cloud1.svg"/>' +
+        '<img id="cloud2" class="weather" src="svg/calendar/cloud2.svg"/>' +
+        '<img id="cloud3" class="weather" src="svg/calendar/cloud3.svg"/>' +
+        '<img id="plane-svg" class="plane" src="svg/calendar/plane.svg"/>';
+    $conteiner.append(template);
 
     $.ajax({
         url: 'https://api.darksky.net/forecast/5b3325c22c6254d2ba685f4a47343d3e/32.530833,-117.02,' + SELECTED_DATE.toString('yyyy-MM-dd') + 'T'+hourFormat+'?exclude=minutely,hourly,daily',
@@ -68,21 +71,21 @@ $('#show-weather-btn').click(function () {
 
         TweenMax.to($conteiner, 0.4, {top: 0, onComplete: function () {
 
+            $('#show-weather-btn').on('click', showWeather);
+
             var t1 = new TimelineLite({onComplete:function() {
-             this.restart();}
-             });
+                this.restart();}
+            });
 
-             t1.to($("#plane-svg"), 8, {rotation: 10, ease: Power0.easeNone});
+            t1.to($("#plane-svg"), 8, {rotation: 10, ease: Power0.easeNone});
 
-             t1.to($("#plane-svg"), 4, {rotation: -10, ease: Power0.easeNone});
+            t1.to($("#plane-svg"), 4, {rotation: -10, ease: Power0.easeNone});
 
-             t1.to($("#plane-svg"), 4, {rotation: 0, ease: Power0.easeNone});
+            t1.to($("#plane-svg"), 4, {rotation: 0, ease: Power0.easeNone});
         }});
 
     });
-
-
-});
+}
 
 function createDrops() {
     if(goRain)
